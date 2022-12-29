42fe12e47ca0e0b29c45ba9a158fc91300dc7950
Jeremy Novak <olepunchy@gmail.com>
2022-12-29 14:41:19 -0600

    Completed ValidAnagram

---
+++ ValidAnagram.Tests/ValidAnagramTests.cs
@@ -0,0 +1,31 @@
+namespace ValidAnagram.Tests;
+
+[TestFixture]
+public class Tests {
+
+    private Solution _solution;
+    
+    [SetUp]
+    public void Setup() {
+    }
+
+    [Test]
+    [TestCase("newyorktimes", "monkeyswrite", true)]
+    [TestCase("elbow", "below", true)]
+    [TestCase("angel", "glean", true)]
+    [TestCase("arc", "car", true)]
+    [TestCase("brag", "grab", true)]
+    [TestCase("cheap", "peach", true)]
+    [TestCase("night", "thing", true)]
+    [TestCase("toxin", "taxi", false)]
+    [TestCase("abbc", "aabc", false)]
+    [TestCase("paper", "reapa", false)]
+    [TestCase("cats", "tocs", false)]
+    [TestCase("wrong", "wong", false)]
+    public void ValidAnagram_WhenCalled_ReturnsCorrectAnswer(
+        string firstString,
+        string secondString,
+        bool correctAnswer) {
+        
+    }
+}
---
+++ ValidAnagram/ValidAnagram.cs
@@ -0,0 +1,31 @@
+﻿namespace ValidAnagram;
+
+public class Solution {
+
+   public bool IsAnagram(string firstString, string secondString) {
+
+      // An anagram requires two strings to be the same length
+      if (firstString.Length != secondString.Length) {
+         return false;
+      }
+
+      int[] store = new int[26];
+
+      for (int i = 0; i < firstString.Length; i++) {
+         // Add the first character
+         store[firstString[i] - 'a']++;
+         // Decrement the second character
+         store[secondString[i] - 'a']--;
+      }
+
+      // All elements should be zero if it is an anagram
+      for (int i = 0; i < store.Length; i++) {
+         if (store[i] != 0) {
+            return false;
+         }
+      }
+
+      return true;
+   } 
+
+}
