83f74b36c2e82d663412c416edb83072a648f08e
Jeremy Novak <olepunchy@gmail.com>
2022-12-29 14:47:54 -0600

    Updated answer to use LINQ expression

--- ValidAnagram/ValidAnagram.cs
+++ ValidAnagram/ValidAnagram.cs
@@ -2,30 +2,45 @@
 
 public class Solution {
 
-   public bool IsAnagram(string firstString, string secondString) {
+    public bool IsAnagram(string firstString, string secondString) {
 
-      // An anagram requires two strings to be the same length
-      if (firstString.Length != secondString.Length) {
-         return false;
-      }
-
-      int[] store = new int[26];
-
-      for (int i = 0; i < firstString.Length; i++) {
-         // Add the first character
-         store[firstString[i] - 'a']++;
-         // Decrement the second character
-         store[secondString[i] - 'a']--;
-      }
-
-      // All elements should be zero if it is an anagram
-      for (int i = 0; i < store.Length; i++) {
-         if (store[i] != 0) {
+        // An anagram requires two strings to be the same length
+        if (firstString.Length != secondString.Length) {
             return false;
-         }
-      }
+        }
 
-      return true;
-   } 
+        int[] store = new int[26];
+
+        for (int i = 0; i < firstString.Length; i++) {
+
+            // Increment the character seen at this position
+            store[firstString[i] - 'a']++;
+            // Decrement the character seen at this position
+            store[secondString[i] - 'a']--;
+        }
+
+        // All elements should be zero if it is an anagram
+      
+        // Hand written for loop example
+        // for (int i = 0; i < store.Length; i++) {
+        //     if (store[i] != 0) {
+        //         return false;
+        //     }
+        // }
+        
+        // foreach loop example
+        // foreach (var t in store) {
+        //     if (t != 0) {
+        //         return false;
+        //     }
+        // }
+
+        // LINQ expression example
+        return store.All(t => t == 0);
+
+        // When using either a for loop or foreach loop, would need to 
+        // return true here.
+        // return true;
+    } 
 
 }
