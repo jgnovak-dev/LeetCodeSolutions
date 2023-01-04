24f8c63234ec6a0fc9e936f39fd818bc23719992
Jeremy Novak <olepunchy@gmail.com>
2023-01-04 09:48:39 -0600

    Completed WordPattern

---
+++ WordPattern.Tests/WordPatternTests.cs
@@ -0,0 +1,24 @@
+namespace WordPattern.Tests;
+
+[TestFixture]
+public class Tests {
+
+    private Solution _solution;
+    
+    [SetUp]
+    public void Setup() {
+        _solution = new Solution();
+    }
+
+    [Test]
+    [TestCase("abba", "dog cat cat dog", true)]
+    [TestCase("abba", "dog cat cat fish", false)]
+    [TestCase("aaaa", "dog cat cat dog", false)]
+    [TestCase("aaa", "aa aa aa aa", false)]
+    public void WordPattern_WhenCalled_ReturnsCorrectAnswer(
+        string letters, 
+        string words, 
+        bool correctAnswer) {
+        
+    }
+}
---
+++ WordPattern/WordPattern.cs
@@ -0,0 +1,41 @@
+﻿namespace WordPattern;
+
+public class Solution {
+
+    public bool WordPattern(string letters, string words) {
+
+        var wordArray = words.Split();
+
+        if (letters.Length != wordArray.Length) {
+            return false;
+        }
+
+        var letterDictionary = new Dictionary<char, string>();
+        var wordDictionary = new Dictionary<string, char>();
+
+        for (int i = 0; i < letters.Length; i++) {
+            var letter = letters[i];
+            var word = wordArray[i];
+
+            if (letterDictionary.ContainsKey(letter)) {
+                if (letterDictionary[letter] != word) {
+                    return false;
+                }
+            } else {
+                letterDictionary[letter] = word;
+            }
+
+            if (wordDictionary.ContainsKey(word)) {
+                if (wordDictionary[word] != letter) {
+                    return false;
+                } 
+            } else {
+                wordDictionary[word] = letter;
+            }
+        }
+
+
+        return true;
+    }
+
+}
